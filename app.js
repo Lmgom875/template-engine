caaf82e19723c61687081b2f9a702eeffebfc9dc
Wuata <lmgom875@gmail.com>
2020-02-06 17:08:17 -0500

    app.js inquirer function

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const inquirer = require("inquirer");
 let team = []
 
 
-function main() {
+function managerInput() {
     inquirer.prompt([
         {
             type: "input",
@@ -21,24 +21,107 @@ function main() {
             name: "email",
             message: "What is you manager E-mail?"
         }, {
-            type: "input",
+            type: "list",
             name: "office",
-            message: "What is you manager office number?"
+            message: "What is you manager office number?",
+            choices: [
+                '101',
+                '102',
+                '103'
+            ]
         }
 
     ]).then(resp => {
-        console.log(resp)
         team.push(new Manager(resp.manager, resp.id, resp.email, resp.office))
-        console.log(team)
+        teamImput();
         // llamar a otra funcion para crear el team
-
     })
 
 
 }
 
+function engineerInput() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "engineer",
+            message: "What is you engineer name?"
+        }, {
+            type: "input",
+            name: "id",
+            message: "What is you engineer ID?"
+        }, {
+            type: "input",
+            name: "email",
+            message: "What is you engineer E-mail?"
+        }, {
+            type: "input",
+            name: "username",
+            message: "What is you engineer GitHub username?",
 
-function menuTeam() {
+        }
+
+    ]).then(resp => {
+        team.push(new Engineer(resp.engineer, resp.id, resp.email, resp.username))
+        teamImput();
+        // llamar a otra funcion para crear el team
+    })
+}
+
+function internInput() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "intern",
+            message: "What is you intern name?"
+        }, {
+            type: "input",
+            name: "id",
+            message: "What is you intern ID?"
+        }, {
+            type: "input",
+            name: "email",
+            message: "What is you intern E-mail?"
+        }, {
+            type: "input",
+            name: "uniName",
+            message: "What is you university name?",
+
+        }
+
+    ]).then(resp => {
+        team.push(new Intern(resp.intern, resp.id, resp.email, resp.uniName))
+        teamImput();
+        // llamar a otra funcion para crear el team
+    })
+}
+
+
+function teamImput() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "role",
+            message: "Add new member role?",
+            choices: [
+                'Engineer',
+                'Intern',
+                'Finish'
+            ]
+        }
+    ]).then(resp => {
+        console.log(resp);
+        if (resp.role === 'Engineer') {
+            engineerInput();
+        } else if (resp.role === 'Intern') {
+            internInput();
+        } else {
+            console.log(team);
+            console.log("end of team creation");
+            console.log("BYE");
+
+        }
+    })
     // crear inter or eng or finish 
 
     //  intern crear 
@@ -53,4 +136,4 @@ function buildHtml() {
     //al final del loop escribir el html con fw.writefile
 }
 
-main();
+managerInput()
